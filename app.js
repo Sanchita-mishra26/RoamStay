a4990b93729006845f45762b659a19f3ce5144ad
sanchitamishra-26 <sanchitamishra0726@gmail.com>
2026-02-04 20:26:17 +0530

    Integrated ejs-mate layouts, fixed image rendering, and added public assets

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require("path");   
-
-
+const { title } = require('process');
+const { privateDecrypt } = require('crypto');
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate"); // Correct (use a hyphen)
 const app = express();
 
 const MongoDB_URL = 'mongodb://127.0.0.1:27017/RoamStay';
@@ -25,7 +27,10 @@ connectDB()
 app.set("view engine", "ejs");
 app.set
 ("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));    
+app.use(express.urlencoded({ extended: true }));   
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/Public")));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -51,6 +56,11 @@ app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 });
+//new route 
+app.get("/listings/new", (req,res) =>{
+    res.render("listings/new.ejs")
+
+})
 
 //show route to show details of a particular listing
 app.get("/listings/:id", async (req, res) => {
@@ -58,6 +68,44 @@ app.get("/listings/:id", async (req, res) => {
     const listing = await Listing.findById(id);    
     res.render("listings/show.ejs", {listing});
 });
+
+//create route
+app.post("/listings", async(req,res) =>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    try {
+        let { id } = req.params;
+        const listing = await Listing.findById(id);
+        if (!listing) {
+            return res.send("Listing not found!"); // Prevents hanging if ID is wrong
+        }
+        res.render("listings/edit.ejs", { listing });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong");
+    }
+});
+
+//update route
+app.put("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+   await Listing.findByIdAndUpdate(id , {...req.body.listing});
+   res.redirect("/listings");
+})
+
+//delete this route
+app.delete("/listings/:id" , async (req,res) =>{
+    let { id } = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    console.log(deleteListing);
+    res.redirect("/listings");
+})
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
